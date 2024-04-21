7f782aea1059f1cf94109dcd4d85c952f8115ecd
Svobodennn <melih.sarac@hotmail.com>
2024-04-21 20:02:42 +0300

    1 - Using routes and controllers

--- basitLaravel/routes/web.php
+++ basitLaravel/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\App;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', App::class); // [App::class, 'index']
+
+Route::get('/aboutus', [App::class, 'aboutus']);
+
+Route::get('/user/{id}', [App::class, 'user']);
